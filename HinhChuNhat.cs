041cc33441c0240230204073a3f893b40a2bf3ad
tluan1 <luanthanh253@gmail.com>
2024-06-17 14:11:40 +0700

    Add project files.

---
+++ HinhChuNhat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_HinhChuNhat_Class
+{
+    internal class HinhChuNhat
+    {
+
+        public double chieuCao; //tính (attribute) biến 
+        public double chieuRong;//tính (attribute) biến
+        public double tinhdientich() // phương thức (methed) hàm
+        {
+            return chieuCao * chieuRong;
+        }
+        public double tinhchuvi()
+        {
+            return (chieuCao + chieuRong)*2;
+        }
+    }
+}
